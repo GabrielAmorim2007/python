4e750c7f3d8fb49d9bb7a25af43808ffd5e5d591
GabrielAmorim2007 <ga.amorim.2007@gmail.com>
2024-05-01 15:43:10 -0400

    tentando 3vez

--- au.py
+++
@@ -1,5 +0,0 @@
-name = (str(input("qual o teu segundo nome em?")))
-if name == "paulão":
-    print("teu cu que é o, tio paulo")
-else:
-    print("ola seu, " + name + "eu quero que você vá toma no seu cu sabia?")
