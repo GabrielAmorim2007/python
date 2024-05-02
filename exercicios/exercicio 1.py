2561099e76647c14c08ded881337daa95828d689
GabrielAmorim2007 <ga.amorim.2007@gmail.com>
2024-05-01 23:01:22 -0400

    trabalho de casa

---
+++ exercicios/exercicio 1.py
@@ -0,0 +1,3 @@
+m = input("fale quantos mettros vc tem? \n")
+n = int(m) * 1000
+print(n)
---
+++ exercicios/exercicio 2.py
@@ -0,0 +1,7 @@
+n1 = int(input(("fale um numero")))
+n2 = int(input(("fale um numero")))
+
+if n1 < n2:
+    print("é o maior numero " + str(n2))
+else :
+    print("é o maior numero " + str(n1))
